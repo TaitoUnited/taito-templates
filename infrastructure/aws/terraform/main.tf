ea8bb73f816eee163bf8b73923294e302db9d941
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2020-07-17 11:53:54 +0300

    feat(aws): use module version 2.2.1

--- infrastructure/aws/terraform/main.tf
+++ infrastructure/aws/terraform/main.tf
@@ -32,7 +32,7 @@ locals {
 
 module "taito_zone" {
   source  = "TaitoUnited/kubernetes-infrastructure/aws"
-  version = "2.2.0"
+  version = "2.2.1"
 
   # Labeling
   name                       = var.taito_zone
