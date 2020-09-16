e13b6536dc6d0adb54cc2bbd9807357f0c16e073
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2020-09-17 01:21:05 +0300

    fix(gcp): db privileges

--- infrastructure/gcp/terraform/database-privileges.tf
+++ infrastructure/gcp/terraform/database-privileges.tf
@@ -5,13 +5,13 @@ provider "postgresql" {
   alias           = "postgresql_0"
   host            = "127.0.0.1"
   port            = 5000
-  username        = local.databases[0].adminUsername
+  username        = local.databases.postgresqlClusters[0].adminUsername
   password        = var.postgresql_0_password
 }
 
 module "postgresql_0" {
   source                     = "TaitoUnited/privileges/postgresql"
-  version                    = "1.0.0"
+  version                    = "1.0.1"
   privileges                 = local.databases.postgresqlClusters[0]
   providers = {
     postgresql = postgresql.postgresql_0
@@ -23,15 +23,14 @@ module "postgresql_0" {
 
 provider "mysql" {
   alias           = "mysql_0"
-  host            = "127.0.0.1"
-  port            = 6000
-  username        = local.databases[0].adminUsername
+  endpoint        = "127.0.0.1:6000"
+  username        = local.databases.mysqlClusters[0].adminUsername
   password        = var.mysql_0_password
 }
 
 module "mysql_0" {
   source                     = "TaitoUnited/privileges/mysql"
-  version                    = "1.0.0"
+  version                    = "1.0.1"
   privileges                 = local.databases.mysqlClusters[0]
   providers = {
     mysql = mysql.mysql_0
