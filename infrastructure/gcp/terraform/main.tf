bde1cfa4bb6380ae5c11218539ec56c762ccc130
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2020-09-24 22:19:11 +0300

    feat(gcp): use kubernetes module 1.15.4

--- infrastructure/gcp/terraform/main.tf
+++ infrastructure/gcp/terraform/main.tf
@@ -114,7 +114,7 @@ data "external" "kubernetes_wait" {
 
 module "kubernetes" {
   source                 = "TaitoUnited/kubernetes/google"
-  version                = "1.15.3"
+  version                = "1.15.4"
 
   # OPTIONAL: Helm app versions
   # ingress_nginx_version  = ...
