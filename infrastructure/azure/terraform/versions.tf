65186bfc625805ab873b172038f4e58be6162804
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2021-06-07 17:52:01 +0300

    fix(azure): avoid azurerm 2.62.0 bug

--- infrastructure/azure/terraform/versions.tf
+++ infrastructure/azure/terraform/versions.tf
@@ -2,9 +2,10 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">=2.53.0"
+      // bug in 2.62.0 https://github.com/terraform-providers/terraform-provider-azurerm/issues/12074
+      version = ">=2.53.0, <2.62.0"
     }
   }
 
-  required_version = ">= 0.14"
+  required_version = ">= 0.15"
 }
