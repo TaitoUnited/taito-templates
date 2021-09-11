25bdd6d00ef433c6ef6157900da1835dbd181511
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2021-09-12 00:31:10 +0300

    fix(azure): update kubernetes module to 1.4.1

--- infrastructure/azure/terraform/main.tf
+++ infrastructure/azure/terraform/main.tf
@@ -117,7 +117,7 @@ module "compute" {
 module "kubernetes" {
   depends_on                 = [ module.admin ]
   source                     = "TaitoUnited/kubernetes/azurerm"
-  version                    = "1.4.0"
+  version                    = "1.4.1"
 
   resource_group_name        = azurerm_resource_group.zone.name
 
