af5bdde9855c1daba3fa0ffd592189841b72d431
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2021-09-12 01:24:01 +0300

    feat(gcp): update kubernetes modules to 2.3.0

--- infrastructure/gcp/terraform/main.tf
+++ infrastructure/gcp/terraform/main.tf
@@ -122,7 +122,7 @@ module "dns" {
 
 module "kubernetes" {
   source                 = "TaitoUnited/kubernetes/google"
-  version                = "2.2.0"
+  version                = "2.3.0"
 
   # OPTIONAL: Helm app versions
   # ingress_nginx_version  = ...
