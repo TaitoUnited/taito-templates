38d70985a05aaf77bd26a1a7672a08691d386093
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2021-10-24 20:37:31 +0300

    fix(aws): remove port from database hosts

--- infrastructure/aws/terraform/outputs.tf
+++ infrastructure/aws/terraform/outputs.tf
@@ -1,9 +1,9 @@
 output "postgres_hosts" {
   description = "Postgres hosts"
-  value       = module.databases.postgres_instance_endpoints
+  value       = module.databases.postgres_instance_addresses
 }
 
 output "mysql_hosts" {
   description = "MySQL hosts"
-  value       = module.databases.mysql_instance_endpoints
+  value       = module.databases.mysql_instance_addresses
 }
