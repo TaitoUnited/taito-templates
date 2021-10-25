f6ebb304aa0cf0f412bd3123c6659296f3baeffa
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2021-10-25 12:46:58 +0300

    feat(aws): add bastion_public_ip output

--- infrastructure/aws/terraform/outputs.tf
+++ infrastructure/aws/terraform/outputs.tf
@@ -7,3 +7,8 @@ output "mysql_hosts" {
   description = "MySQL hosts"
   value       = module.databases.mysql_instance_addresses
 }
+
+output "bastion_public_ip" {
+  description = "Bastion public ip"
+  value       = module.network.bastion_public_ip
+}
