bfcb0a7f4cef109b2d8eefde530434438271bc0c
keskiju <jukka.keskiluopa@taitounited.fi>
2024-09-06 14:58:48 +0300

    feat(gcp): use kube module v2.9.0

--- infrastructure/gcp/terraform/main.tf
+++ infrastructure/gcp/terraform/main.tf
@@ -122,7 +122,7 @@ module "dns" {
 
 module "kubernetes" {
   source                 = "TaitoUnited/kubernetes/google"
-  version                = "2.8.0"
+  version                = "2.9.0"
 
   # OPTIONAL: Helm app versions
   # ingress_nginx_version  = ...
