a545796a4d5117f3ac5ec92de69bec51418d6648
keskiju <jukka.keskiluopa@taitounited.fi>
2024-12-08 14:07:42 +0200

    feat(gcp): compute module 1.2.1

--- infrastructure/gcp/terraform/main.tf
+++ infrastructure/gcp/terraform/main.tf
@@ -105,7 +105,7 @@ module "admin" {
 
 module "compute" {
   source              = "TaitoUnited/compute/google"
-  version             = "1.2.0"
+  version             = "1.2.1"
 
   project_id          = (
     var.first_run
